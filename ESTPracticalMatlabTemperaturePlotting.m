0ec38b67f0e499d4b3a3ffc56ab82c19437fa572
AlbertdeBeaufort <a.m.d.beaufort@student.tue.nl>
2024-05-28 13:36:18 +0200

    Update ESTPracticalMatlabTemperaturePlotting.m
    Conversion ms to s and added units to axes lables

--- ESTPracticalMatlabTemperaturePlotting.m
+++ ESTPracticalMatlabTemperaturePlotting.m
@@ -1,14 +1,13 @@
-
 fileName = 'TemperatureDataMeasurementX.txt'; % Enter name of data file to plot
 data = dlmread(fileName);
 
 
-time = data(:, 1); % Setting up the axes
+time = data(:, 1)/1000; % Setting up the axes and vonverting milliseconds to seconds
 temperature = data(:, 2);
 
 
 plot(time, temperature); %Plotting the actual graph
-xlabel('Time');
-ylabel('Temperature');
+xlabel('Time (s)');
+ylabel('Temperature (C)');
 title('Temperature vs Time');
 grid on;
